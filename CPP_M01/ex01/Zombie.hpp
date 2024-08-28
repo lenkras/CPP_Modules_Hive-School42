a042881c6dce801100702d9076b329ce31fd50db
Elena Polkhovski <epolkhov@c1r6p6.hive.fi>
2024-08-28 16:50:48 +0300

    add start of ex01

---
+++ CPP_M01/ex01/Zombie.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epolkhov <epolkhov@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-08-28 13:24:30 by epolkhov          #+#    #+#             */
+/*   Updated: 2024-08-28 13:24:30 by epolkhov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <string>
+#include <iostream>
+
+class Zombie
+{
+	private:
+		std::string  name;
+	public:
+		Zombie(std::string name);
+		~Zombie();
+
+		void announce(void);
+};
+
+
+#endif
