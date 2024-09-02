1dd1b72a24419e488cb7e18a881ffc7434ef2841
Elena Polkhovski <epolkhov@c3r4p2.hive.fi>
2024-09-02 19:34:48 +0300

    add return in case of error

--- CPP_M00/ex00/megaphone.cpp
+++ CPP_M00/ex00/megaphone.cpp
@@ -15,7 +15,10 @@
 int	main(int argc, char **argv)
 {
 	if (argc == 1)
+	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << "\n";
+		return (1);
+	}
 	else
 	{
 		for (int i{1}; i < argc; i++)
